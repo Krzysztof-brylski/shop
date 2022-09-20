7f4a112663d9c8787555d5f18adc34f739260468
krzysztof-brylski1 <krzysztof.brylski1@o2.pl>
2022-09-20 02:46:24 +0200

    initial commit

---
+++ app/Http/Controllers/HelloControler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloControler extends Controller
+{
+    public function show(){
+        return view('hello');
+    }
+
+    public function proces(){
+
+    }
+
+}
